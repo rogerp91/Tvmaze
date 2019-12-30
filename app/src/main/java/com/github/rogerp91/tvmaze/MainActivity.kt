477317ad10164ca4962d2179a6009a58f419d25e
rogerp91 <roger.patino@mercadolibre.com>
2019-12-30 08:17:25 -0300

    add .travis

--- app/src/main/java/com/github/rogerp91/tvmaze/MainActivity.kt
+++ app/src/main/java/com/github/rogerp91/tvmaze/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.rogerp91.tvmaze
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
